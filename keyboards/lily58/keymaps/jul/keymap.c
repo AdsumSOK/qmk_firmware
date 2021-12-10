a9afba9985e4000c08d4c42c6ed5425e58048f83
Adsum <julfouillot@yahoo.com>
2021-12-10 11:56:25 +0100

    Ajout du layer media

--- keyboards/lily58/keymaps/jul/keymap.c
+++ keyboards/lily58/keymaps/jul/keymap.c
@@ -25,13 +25,18 @@ enum layers {
     _QWERTY,
     _LOWER,
     _RAISE,
-    _ADJUST
+    _GAME
 };
 
 #define QWERTY MO(_QWERTY)
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
-#define ADJUST MO(_ADJUST)
+#define GAME MO(_GAME)
+
+#ifdef RGBLIGHT_ENABLE
+#define COLORSURB 0, 255, 255   // RED
+#define COLORSURB2 51, 87, 100  // WHITE
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -53,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                              KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI
 ),
 /* LOWER
@@ -100,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
-/* ADJUST
+/* GAME
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -114,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [_ADJUST] = LAYOUT(
+  [_GAME] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
@@ -124,11 +129,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+    //state = update_tri_layer_state(state, _RAISE, _LOWER, _GAME);
 #ifdef RGBLIGHT_ENABLE// RGBLIGHT_ENABLE
     rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
     rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _GAME));
     rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
 #endif // RGBLIGHT_ENABLE
     return state;
@@ -140,29 +145,15 @@ const rgblight_segment_t PROGMEM my_qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 );
 
 const rgblight_segment_t PROGMEM my_lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {6,4, HSV_BLUE},
-    {12,4,HSV_BLUE}
+    {0,70, HSV_PURPLE},
+    {11,2, HSV_BLUE},
+    {24,1, HSV_BLUE},
+    {65,2, HSV_BLUE},
+    {30,2, HSV_BLUE}
 );
 
 const rgblight_segment_t PROGMEM my_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-#define COLORBACK 170, 255, 255
-#define COLORSURB 0, 255, 255
-#define COLORSURB2 51, 87, 100
-/*
-    {0,20, COLORBACK},// Light 20 LEDs, starting with LED 0
-    {20,1, COLORSURB},
-    {21,5, COLORBACK},
-    {26,3, COLORSURB},
-    {29,6, COLORBACK},
-    {36,14, COLORBACK},
-    {50,6, COLORSURB},
-    {56,6, COLORBACK},
-    {62,3, COLORSURB},
-    {65,2, COLORBACK},
-    {67,1, COLORSURB},
-    {68,2, COLORBACK}
-*/
-    {0,70, COLORBACK},// Light 20 LEDs, starting with LED 0
+    {0,70, HSV_BLUE},// Light 70 LEDs, starting with LED 0
     {20,1, COLORSURB},
     {26,3, COLORSURB},
     {50,6, COLORSURB},
@@ -173,39 +164,19 @@ const rgblight_segment_t PROGMEM my_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 
 );
 
-const rgblight_segment_t PROGMEM my_adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {6,4, HSV_RED}
+const rgblight_segment_t PROGMEM my_game_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0,70, HSV_GREEN},// Light 70 LEDs, starting with LED 0
+    {30,1, COLORSURB2},
+    {50,1, COLORSURB},
+    {54,3, COLORSURB}
 );
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_qwerty_layer,
     my_lower_layer,
     my_raise_layer,
-    my_adjust_layer
+    my_game_layer
 );
-
-void change_keyboard_color_for_layer(void){
-
-      switch (get_highest_layer(layer_state)) {
-         case _QWERTY:
-            rgblight_sethsv_noeeprom(HSV_ORANGE);
-            break;
-        case _LOWER:
-            rgblight_sethsv_noeeprom(HSV_PURPLE);
-            break;
-        case _RAISE:
-            rgblight_sethsv_noeeprom(HSV_GREEN);
-            rgblight_sethsv_at(HSV_RED, 20);
-            rgblight_sethsv_range(HSV_RED, 26,29);
-            break;
-        case _ADJUST:
-            rgblight_sethsv_noeeprom(HSV_CYAN);
-            break;
-        default:
-            rgblight_sethsv_noeeprom(HSV_RED);
-     }
-
-}
 
 layer_state_t default_user(layer_state_t state) {
     rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
@@ -216,7 +187,7 @@ layer_state_t default_user(layer_state_t state) {
 #endif // RGBLIGHT_ENABLE
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
-#ifdef OLED_ENABLE
+#ifdef OLED_ENABLEo
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
@@ -323,7 +294,7 @@ void render_default_layer_state(void) {
         case _RAISE:
             oled_write_P(PSTR("PLUS"), false);
             break;
-        case _ADJUST:
+        case _GAME:
             oled_write_P(PSTR("GAME"), false);
             break;
         default:
@@ -455,10 +426,8 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
 #ifdef RGBLIGHT_ENABLE
 void keyboard_post_init_user(void) {
-    //rgblight_enable_noeeprom(); // Enables RGB, without saving settings
-    rgblight_layers = my_rgb_layers;
+   rgblight_layers = my_rgb_layers;
     rgblight_sethsv_noeeprom(HSV_ORANGE);
     rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
-    //rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
 #endif
