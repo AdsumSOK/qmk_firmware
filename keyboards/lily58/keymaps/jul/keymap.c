18974301280920cc21593360c39d52dbecd3173e
Adsum <julfouillot@yahoo.com>
2022-01-17 10:12:27 +0100

    Reamenagement du layer 2 / Invierson End et Home

--- keyboards/lily58/keymaps/jul/keymap.c
+++ keyboards/lily58/keymaps/jul/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_RAISE] = LAYOUT(
   KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TRNS,  KC_SLCK, KC_END,  KC_HOME,  KC_INS, KC_NO,                     KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_PMNS, KC_F12,
+  KC_TRNS,  KC_SLCK, KC_HOME,  KC_END,  KC_INS, KC_NO,                     KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_PMNS, KC_F12,
   KC_TRNS,  KC_PAUS, KC_PGUP, KC_UP,   KC_PGDN, KC_NO,                    KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PSLS, TO(3),
   KC_TRNS,  KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_CAPS, KC_NLCK,  KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_NO, KC_TRNS,
                                TO(0), KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_P0,   KC_PDOT, _______
